a71712e5ec5438e282692e57e6581dbfc3c2792f
saadu <saadu@SKIPAD>
2023-05-23 11:20:54 -0400

    Add project files.

---
+++ testing/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    internal class Class1 : BankAccount
+    {
+        public Class1(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+
+        }
+    }
+
+    
+    
+}
